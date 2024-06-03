11f2d5b49939da5c002d0780721cf99237be4f36
Giorgi Kutubidze <ffs@gmail.com>
2024-06-03 14:34:07 +0400

    fix route

--- src/app/utils/authUtils.ts
+++ src/app/utils/authUtils.ts
@@ -9,7 +9,7 @@ export const handleLogIn = async () => {
   supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${baseUrl}/auth/callback`,
+      redirectTo: `${baseUrl}/auth/callback/route`,
     },
   });
 };
