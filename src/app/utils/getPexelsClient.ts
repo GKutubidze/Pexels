a693f85e15d6aac88ca92dd2374e67e3034d83ba
Giorgi Kutubidze <ffs@gmail.com>
2024-06-04 15:49:32 +0400

    fix varaibless

--- src/app/utils/getPexelsClient.ts
+++ src/app/utils/getPexelsClient.ts
@@ -1,16 +1,18 @@
+// src/utils/pexelsClient.ts
 import { createClient } from "pexels";
 
 export const getPexelsClient = () => {
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY as string | undefined;
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  console.log("Pexels API Key:", apiKey); // Log for debugging
+
   if (!apiKey) {
     console.error(
       "Pexels API key is not set. Please set the NEXT_PUBLIC_API_KEY environment variable."
     );
-    // Optionally, throw an error or return a default client
     throw new Error(
       "Pexels API key is required. Please set the NEXT_PUBLIC_API_KEY environment variable."
     );
-    // return null; // Uncomment this line if you prefer returning null instead of throwing an error
   }
+
   return createClient(apiKey);
 };
