c04312efc7c6ae62cb7c952e47f97d4aa8a2ccc6
Giorgi Kutubidze <ffs@gmail.com>
2024-06-04 16:17:14 +0400

    fix browserclients

--- src/app/utils/authUtils.ts
+++ src/app/utils/authUtils.ts
@@ -4,14 +4,25 @@ export const handleLogIn = async () => {
   const supabase = supabaseBrowser();
 
   // Determine the base URL
-  const baseUrl ="https://pexels-tawny.vercel.app"; // Replace with your actual Vercel app URL
-  
-  supabase.auth.signInWithOAuth({
-    provider: "google",
-    options: {
-      redirectTo: `${baseUrl}/auth/callback`,
-    },
-  });
+  const baseUrl = "https://pexels-tawny.vercel.app"; // Replace with your actual Vercel app URL
+
+  try {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${baseUrl}src/app/auth/callback/`,
+      },
+    });
+
+    if (error) {
+      console.error("Error during sign in:", error);
+      // Handle the error appropriately
+    } else {
+      console.log("Sign in successful");
+    }
+  } catch (err) {
+    console.error("Unexpected error during sign in:", err);
+  }
 };
 
 export const handleLogOut = async () => {
