8e7fe2954750fd2924fcb50410c646938b034835
Giorgi Kutubidze <ffs@gmail.com>
2024-06-04 16:31:45 +0400

    remove logs

--- src/app/utils/getPexelsClient.ts
+++ src/app/utils/getPexelsClient.ts
@@ -3,7 +3,6 @@ import { createClient } from "pexels";
 
 export const getPexelsClient = () => {
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-  console.log("Pexels API Key:", apiKey); // Log for debugging
 
   if (!apiKey) {
     console.error(
