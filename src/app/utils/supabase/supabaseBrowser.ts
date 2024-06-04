9d586f6a434f217984103cfc592f627699314772
Giorgi Kutubidze <ffs@gmail.com>
2024-06-04 14:58:12 +0400

    fix varss

--- src/app/utils/supabase/supabaseBrowser.ts
+++ src/app/utils/supabase/supabaseBrowser.ts
@@ -2,5 +2,8 @@ import { createBrowserClient, createServerClient } from "@supabase/ssr";
 import React from "react";
 
 export default function supabaseBrowser() {
-  return createBrowserClient(process.env.SUPABASE_URL!, process.env.ANON_KEY!);
+  return createBrowserClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!
+  );
 }
