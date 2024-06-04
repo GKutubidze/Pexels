329cd6ac18f9b118de29dcffd2d30c5a7ed80210
Giorgi Kutubidze <ffs@gmail.com>
2024-06-04 15:56:21 +0400

    fix browserclient

--- src/app/utils/supabase/supabaseBrowser.ts
+++ src/app/utils/supabase/supabaseBrowser.ts
@@ -2,14 +2,17 @@ import { createBrowserClient } from "@supabase/ssr";
 import React from "react";
 
 export default function supabaseBrowser() {
+  // Read environment variables
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+  // Ensure environment variables are set
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error(
       "Supabase URL and Anonymous Key must be set in environment variables"
     );
   }
 
+  // Create and return Supabase client
   return createBrowserClient(supabaseUrl, supabaseAnonKey);
 }
