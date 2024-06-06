29fca82749c40b32945a7bf6964f7f08b994ff13
Giorgi Kutubidze <ffs@gmail.com>
2024-06-07 00:04:21 +0400

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,33 @@
-'use client'
- import { Plus_Jakarta_Sans } from "next/font/google";
+"use client";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { MediaContextProvider } from "./Context/MediaContext";
 import { Metadata } from "next";
-  
+
 const jackarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
-
-  const metadata: Metadata = {
-  title: "Pexels",
-  description: "Pexels.com Clone",
+const metadata: Metadata = {
+  title: "SnapStock",
+  description:
+    "SnapStock offers a vast collection of high-quality, free stock photos and videos for personal and commercial use. Discover stunning images and footage to enhance your creative projects, all available for download without any cost.",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>)
-
-{
-
- 
+}>) {
   return (
     <MediaContextProvider>
-     <html lang="en">
-     <head>
-           <title>{metadata.title as string}</title>
-          <meta name="description" content={metadata.description as string} />       
+      <html lang="en">
+        <head>
+          <title>{metadata.title as string}</title>
+          <link rel="icon" href="/images/favicon.ico" sizes="any" />
+          <meta name="description" content={metadata.description as string} />
         </head>
 
-      <body className={jackarta.className}>{children}</body>
-    </html>
+        <body className={jackarta.className}>{children}</body>
+      </html>
     </MediaContextProvider>
-   );
+  );
 }
