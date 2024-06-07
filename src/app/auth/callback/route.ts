dd76a9ab2f7706aa1df68058dad74f7868b5bb1f
Giorgi Kutubidze <ffs@gmail.com>
2024-06-07 19:44:12 +0400

    change url

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -30,7 +30,7 @@ export async function GET(request: Request) {
     );
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`https://pexels-tawny.vercel.app${next}`);
+      return NextResponse.redirect(`https://snap-stock.vercel.app${next}`);
     }
   }
 
